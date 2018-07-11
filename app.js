c2d7fdbc088a6b388a5a2e31d602a775f18fcecf
jverneaut <jverneaut@gmail.com>
2018-07-12 01:58:38 +0200

    Add morgan logging

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 import usersRoutes from './routes/users';
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
