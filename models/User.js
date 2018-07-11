5d65cd4443dbdfcbea3c5b09479bfba8b1678a48
jverneaut <jverneaut@gmail.com>
2018-07-12 01:00:51 +0200

    Add User model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Post',
+    },
+  ],
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Comment',
+    },
+  ],
+});
+
+export default model('User', userSchema);
