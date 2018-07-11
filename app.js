cea2b7531980c9ee7ba5e354dc6d9ee56e0158b5
jverneaut <jverneaut@gmail.com>
2018-07-12 01:35:49 +0200

    Add body-parser middleware

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 import usersRoutes from './routes/users';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Hello, World!' });
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(usersRoutes);
 
