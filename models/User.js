18b302605cdc8165836914d71546746a40640347
jverneaut <jverneaut@gmail.com>
2018-07-12 01:53:28 +0200

    Add bcrypt

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const { Schema } = mongoose;
 
@@ -25,6 +26,18 @@ const userSchema = new Schema({
       ref: 'Comment',
     },
   ],
+});
+
+// eslint-disable-next-line
+userSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default mongoose.model('User', userSchema);
