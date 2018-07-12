cf94089dbdf3d348ce323b9f7fe7490f04c113b0
jverneaut <jverneaut@gmail.com>
2018-07-12 12:55:15 +0200

    Add express-session

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import session from 'express-session';
 
 import usersRoutes from './routes/users';
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(session({ secret: 'work hard', saveUninitialized: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
