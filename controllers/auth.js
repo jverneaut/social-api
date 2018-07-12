aefd09dc58d5ebc25ca269add743b7977748799a
jverneaut <jverneaut@gmail.com>
2018-07-12 13:18:01 +0200

    Save userId in session

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,5 +4,6 @@ exports.signup = async (req, res) => {
   const { email, password } = req.body;
   const newUser = new User({ email, password });
   await newUser.save();
+  req.session.userId = newUser._id;
   res.json(newUser);
 };
