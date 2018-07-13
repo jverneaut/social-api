3de5142c740f191c9de27a6988692ba7b3ca758a
jverneaut <jverneaut@gmail.com>
2018-07-13 16:57:54 +0200

    Add /users/current_user endpoint

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,11 @@
 import User from '../models/User';
 
+exports.getCurrentUser = async (req, res) => {
+  const { userId } = req.session;
+  const user = await User.findById(userId);
+  res.status(200).json(user);
+};
+
 exports.findAll = async (req, res) => {
   const users = await User.find();
   res.json(users);
--- routes/users.js
+++ routes/users.js
@@ -6,5 +6,6 @@ import usersControllers from '../controllers/users';
 const Router = express.Router();
 
 Router.get('/users', requireLogin, usersControllers.findAll);
+Router.get('/users/current_user', requireLogin, usersControllers.getCurrentUser);
 
 export default Router;
