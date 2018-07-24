c0d9de72420297b91ee19d4907b60c4f54aff545
jverneaut <jverneaut@gmail.com>
2018-07-24 23:26:56 +0200

    Refactor auth tests

--- test/auth.test.js
+++ test/auth.test.js
@@ -4,97 +4,117 @@ import request from 'supertest';
 
 import app from '../app';
 
-const agent = request.agent(app);
+const dummyUser = {
+  email: 'johndoe@gmail.com',
+  password: '1234',
+};
 
-describe('User authentication', () => {
+const userAgent = request.agent(app);
+
+const createUser = user => request(app)
+  .post('/auth/signup')
+  .send(user);
+
+const loginUser = user => userAgent
+  .post('/auth/login')
+  .send(user);
+
+describe('Create user and login/logout', () => {
   before((done) => {
+    mongoose.connect(process.env.MONGO_TEST_URI, { useNewUrlParser: true });
+    mongoose.connection.once('open', () => done());
+  });
+
+  before((done) => {
+    mongoose.connection.db.dropDatabase(() => done());
+  });
+
+  beforeEach((done) => {
     mongoose.connection.collections.users.drop(() => done());
   });
 
+  beforeEach((done) => {
+    mongoose.connection.collections.users.ensureIndex({ email: 1 }, { unique: true });
+    done();
+  });
+
+  afterEach((done) => {
+    mongoose.connection.collections.users.drop(() => done());
+  });
+
+  afterEach((done) => {
+    mongoose.connection.collections.users.ensureIndex({ email: 1 }, { unique: true });
+    done();
+  });
+
+  it('should not access protected ressources if not logged in', (done) => {
+    request(app)
+      .get('/users')
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        done();
+      });
+  });
+
   it('should create a user', (done) => {
-    agent
-      .post('/auth/signup')
-      .send({ email: 'johndoe@gmail.com', password: '1234' })
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        done();
-      });
+    createUser(dummyUser).end((err, res) => {
+      expect(res.status).to.equal(200);
+      done();
+    });
   });
 
-  it('should fail user creation if email already used', (done) => {
-    agent
-      .post('/auth/signup')
-      .send({ email: 'johndoe@gmail.com', password: '1234' })
-      .end((err, res) => {
-        expect(res.status).to.equal(409);
+  it('should not create duplicate user', (done) => {
+    createUser(dummyUser).end((err1, res1) => {
+      expect(res1.status).to.equal(200);
+      createUser(dummyUser).end((err2, res2) => {
+        expect(res2.status).to.equal(409);
         done();
       });
+    });
   });
 
-  it('should fail login if wrong password', (done) => {
-    agent
-      .post('/auth/login')
-      .send({ email: 'johndoe@gmail.com', password: 'wrong password' })
-      .end((err, res) => {
-        expect(res.status).to.equal(401);
+  it('should login created user', (done) => {
+    createUser(dummyUser).end((err1, res1) => {
+      expect(res1.status).to.equal(200);
+      loginUser(dummyUser).end((err2, res2) => {
+        expect(res2.status).to.equal(200);
         done();
       });
+    });
   });
 
-  it('should log user and set session cookie', (done) => {
-    agent
-      .post('/auth/login')
-      .send({ email: 'johndoe@gmail.com', password: '1234' })
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        done();
+  it('should access protected ressources if logged in', (done) => {
+    createUser(dummyUser).end((err1, res1) => {
+      expect(res1.status).to.equal(200);
+      loginUser(dummyUser).end((err2, res2) => {
+        expect(res2.status).to.equal(200);
+        userAgent
+          .get('/users')
+          .end((err3, res3) => {
+            expect(res3.status).to.equal(200);
+            done();
+          });
       });
+    });
   });
 
-  it('should show all users once logged in', (done) => {
-    agent
-      .get('/users')
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body).to.be.an('array');
-        done();
+  it('should logout and not access protected ressources', (done) => {
+    createUser(dummyUser).end((err1, res1) => {
+      expect(res1.status).to.equal(200);
+      loginUser(dummyUser).end((err2, res2) => {
+        expect(res2.status).to.equal(200);
+        userAgent
+          .get('/auth/logout')
+          .end((err3, res3) => {
+            expect(res3.status).to.equal(200);
+            userAgent
+              .get('/users')
+              .end((err4, res4) => {
+                expect(res4.status).to.equal(401);
+                done();
+              });
+          });
       });
-  });
-
-  it('should show current user once logged in', (done) => {
-    agent
-      .get('/users/current_user')
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.body).to.be.an('object');
-        done();
-      });
-  });
-
-  it('should log out user', (done) => {
-    agent
-      .get('/auth/logout')
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        done();
-      });
-  });
-
-  it('should forbid access to users once logged out', (done) => {
-    agent
-      .get('/users')
-      .end((err, res) => {
-        expect(res.status).to.equal(401);
-        done();
-      });
-  });
-
-  it('should forbid access to current user once logged out', (done) => {
-    agent
-      .get('/users/current_user')
-      .end((err, res) => {
-        expect(res.status).to.equal(401);
-        done();
-      });
+    });
   });
 });
--- test/helper.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-before((done) => {
-  mongoose.connect(process.env.MONGO_TEST_URI, { useNewUrlParser: true });
-  mongoose.connection
-    .once('open', () => done())
-    .on('error', err => console.warn(err));
-});
